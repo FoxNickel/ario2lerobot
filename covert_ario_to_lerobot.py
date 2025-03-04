10a0ce5d7195db6be3767b1cf36c0163dfd741d6
lingyuhuang <nickelfox.foxmail.com>
2025-03-04 06:19:49 +0000

    加进度条

--- covert_ario_to_lerobot.py
+++ covert_ario_to_lerobot.py
@@ -4,6 +4,7 @@ import numpy as np
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 import tyro
 import os
+from tqdm import tqdm
 from tools.read_episode_data import get_episode_data
 
 
@@ -18,9 +19,9 @@ def convert_one_episode(task, episode, episode_path):
         right_wrist_image_frames,
     ) = get_episode_data(episode_path)
 
-    print(
-        f"joint_position shape: {joint_position.shape}, image_high_frames: {len(image_high_frames)}, left_wrist_image_frames: {len(left_wrist_image_frames)}, right_wrist_image_frames: {len(right_wrist_image_frames)}"
-    )
+    # print(
+    #     f"joint_position shape: {joint_position.shape}, image_high_frames: {len(image_high_frames)}, left_wrist_image_frames: {len(left_wrist_image_frames)}, right_wrist_image_frames: {len(right_wrist_image_frames)}"
+    # )
 
     output_path = Path("datasets/lerobot/conversion")
     if output_path.exists():
@@ -64,7 +65,7 @@ def convert_one_episode(task, episode, episode_path):
         image_writer_processes=5,
     )
 
-    for i in range(joint_position.shape[0]):
+    for i in tqdm(range(joint_position.shape[0]), desc=f"Processing {task} {episode}"):
         dataset.add_frame(
             {
                 "image_high": image_high_frames[i],
@@ -76,7 +77,7 @@ def convert_one_episode(task, episode, episode_path):
         )
     dataset.save_episode(task=task)
     dataset.consolidate(run_compute_stats=False)
-    print(f"Processed {task} {episode}")
+    # print(f"Processed {task} {episode}")
 
 
 def process_all_episodes():
@@ -89,9 +90,9 @@ def process_all_episodes():
         if os.path.isdir(os.path.join(origin_data_root_dir, task))
     ]
 
-    for task in tasks:
+    for task in tqdm(tasks, desc="Processing tasks"):
         task_path = os.path.join(origin_data_root_dir, task)
-        print(f"Processing task: {task}")
+        # print(f"Processing task: {task}")
 
         episodes = [
             episode
@@ -100,11 +101,11 @@ def process_all_episodes():
         ]
         episodes.sort(key=lambda x: int(x.split("-")[-1]))  # 按序号排序
 
-        for episode in episodes:
+        for episode in tqdm(episodes, desc=f"Processing episodes in {task}"):
             episode_path = os.path.join(task_path, episode)
-            print(f"Processing episode: {episode}")
+            # print(f"Processing episode: {episode}")
             # print(f"Episode path: {episode_path}")
-            # convert_one_episode(task, episode, episode_path)
+            convert_one_episode(task, episode, episode_path)
 
 
 def process_one_episode():
